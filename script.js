bd51f7cdbf90c1dcd7276cc18ef20442f676be5b
dperezc21 <davierperez11.2@gmail.com>
2022-06-28 00:51:21 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,123 @@
+
+
+async function getStocks() {
+    try {
+        const response = await fetch('http://localhost:3000/getStocks');
+        const data = await response.json();
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function updateStock(symbol) {
+    try {
+        const response = await fetch('http://localhost:3000/updateStockPrice?symbol=' + symbol);
+        const data = await response.json();
+        if (data) {
+            location.reload();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function deleteStock(symbol) {
+    try {
+        const response = await fetch('http://localhost:3000/deleteStock?symbol=' + symbol);
+        const data = await response.json();
+        if (data) {
+            location.reload();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function addStock() {
+    const inputSymbol = document.getElementById('inputSymbol').value
+    console.log(inputSymbol)
+    try {
+        const response = await fetch('http://localhost:3000/addStock?symbol=' + inputSymbol);
+        const data = await response.json();
+        if (data) {
+            location.reload();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+async function addStocksView() {
+    const divStocks = document.querySelector('#stocks');
+    const getStock = await getStocks();
+    for (const iterator of getStock) {
+        if(iterator == null) continue;
+        const div = document.createElement("div");
+        div.className = "stock";
+
+        const logo = document.createElement("IMG");
+        const nameCompany = document.createElement('LABEL')
+        const price = document.createElement('LABEL')
+        const buttonDeleteStock = document.createElement('button');
+        const buttonUpdateStock = document.createElement('button');
+
+        price.className = "label"
+        nameCompany.className="label"
+
+        buttonUpdateStock.addEventListener('click', async () => {
+            updateStock(iterator.symbol);
+        })
+        buttonDeleteStock.addEventListener('click', async () => {
+            deleteStock(iterator.symbol);
+        })
+        logo.setAttribute('src', iterator.url);
+        nameCompany.innerHTML = iterator.companyName
+        price.innerHTML = iterator.latestPrice
+
+        if (iterator.statePrice == "-") {
+            price.style.color = "red"
+        } else if (iterator.statePrice == "+") {
+            price.style.color = rgb(5, 139, 5);
+        } else {
+            price.style.color = "green"
+        }
+
+        logo.setAttribute('width', '70')
+        logo.setAttribute('height', '70')
+
+        const d = document.createElement("IMG");
+        const u = document.createElement("IMG");
+        d.setAttribute('src', "./img/delete.png");
+        u.setAttribute('src', "./img/reload.png");
+
+        buttonDeleteStock.appendChild(d)
+        buttonUpdateStock.appendChild(u)
+
+        const divImage = document.createElement("div");
+        const divNameCompany = document.createElement("div");
+        const divPrice = document.createElement("div");
+        const divButton = document.createElement("div");
+
+        divImage.appendChild(logo)
+        div.appendChild(divImage)
+
+        divNameCompany.appendChild(nameCompany)
+        div.appendChild(divNameCompany)
+
+        divPrice.appendChild(price)
+        div.appendChild(divPrice)
+
+        divButton.appendChild(buttonDeleteStock)
+        div.appendChild(divButton)
+
+        divButton.appendChild(buttonUpdateStock)
+        div.appendChild(divButton)
+
+        divStocks.appendChild(div)
+    }
+    
+
+}
+
+window.onload = addStocksView()
+
